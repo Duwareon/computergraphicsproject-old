912014c44d85564a186e86333603d1b6e3f10cf5
duwareon <duwareon@gmail.com>
2022-07-20 11:58:27 -0400

    got to the point of drawing wire tris

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
-const WIDTH: u32 = u8::MAX as u32;
-const HEIGHT: u32 = u8::MAX as u32;
+const WIDTH: u32 = 512;
+const HEIGHT: u32 = 512;
 
 fn put_pixel(x: u32, y: u32, col: [u8; 4], frame: &mut [u8]) {
     let pixelindex: u64 = (y as u64) * (WIDTH as u64) + (x as u64);
@@ -17,32 +17,81 @@ fn put_pixel(x: u32, y: u32, col: [u8; 4], frame: &mut [u8]) {
     }
 }
 
-fn draw(frame: &mut [u8], time: Instant) {
-    let now = Instant::now();
-    let elapsed = now.duration_since(time).subsec_millis();
+/*fn map(f: impl Fn(i32) -> i32, i: i32, xoff: i32, yoff: i32, a: i32, b: i32) -> u32 {
+    let x = f((i - xoff) / a) * b;
+    let y = || -> i32 { yoff - x.max(0 - HEIGHT as i32 / 2).min(HEIGHT as i32 / 2) };
+    return y() as u32;
+}
 
-    let f = |i: f32| -> f32 { i.sin() }; // + (elapsed as f32 / 160.0)).sin() };
-    let f8 = |i: u8| -> u8 { ((f((i as f32) / 10.0) + 1.0) * 20.0) as u8 };
+fn heart(i: i32) -> i32 {
+    let j = i as f32;
+    return (j.abs().powf(2.0 / 3.0)
+        + (8.0 - j.abs().powf(2.0)).abs().powf(1.0 / 2.0) * (16.0 * 3.1415926 * j).sin())
+        as i32;
+}*/
 
-    frame.iter_mut().for_each(|m| *m = 0);
-
-    for i in 0..WIDTH {
-        //println!("f({}) = {}", i, f(i as f32));
-        put_pixel(i, (0x80 - f8(i as u8)) as u32, [0xffu8; 4], frame);
-    }
-
-    //println!("{:?}", time.elapsed());
-    /*for i in 0..WIDTH {
-        for j in 0..HEIGHT {
-            let r: u8 = i as u8;
-            let g: u8 = j as u8;
-            let b: u8 = 0xa0;
-            let a: u8 = ((elapsed / 10) % 255) as u8;
-            put_pixel(i, j, [r, g, b, a], frame);
+fn interpolate(i0: u32, d0: f32, i1: u32, d1: f32) -> Vec<f32> {
+    if !(i0 == i1) {
+        let mut values = Vec::new();
+        let a = (d1 - d0) / (i1 - i0) as f32;
+        let mut d = d0;
+        for i in i0..i1 {
+            values.push(d);
+            d = d + a;
         }
-        //println!("c{}", i)
-    }*/
-    //println!("\njob done");
+        return values;
+    } else {
+        return vec![d0];
+    }
+}
+
+fn draw_line(p0: [i32; 2], p1: [i32; 2], col: [u8; 4], frame: &mut [u8]) {
+    let mut x0 = p0[0];
+    let mut y0 = p0[1];
+    let mut x1 = p1[0];
+    let mut y1 = p1[1];
+
+    println!("C");
+    if (x1 - x0).abs() > (y1 - y0).abs() {
+        println!("A");
+        // Make sure x0 < x1
+        if x0 > x1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }
+        let ys = interpolate(x0 as u32, y0 as f32, x1 as u32, y1 as f32);
+        for x in x0..x1 {
+            put_pixel(x as u32, ys[(x - x0) as usize] as u32, col, frame);
+        }
+    } else {
+        println!("B");
+        // Make sure y0 < y1
+        if y0 > y1 {
+            std::mem::swap(&mut x0, &mut x1);
+            std::mem::swap(&mut y0, &mut y1);
+        }
+        let xs = interpolate(y0 as u32, x0 as f32, y1 as u32, x1 as f32);
+        for y in y0..y1 {
+            put_pixel(xs[(y - y0) as usize] as u32, y as u32, col, frame);
+        }
+    }
+}
+
+fn draw_wire_triangle(p0: [i32; 2], p1: [i32; 2], p2: [i32; 2], col: [u8; 4], frame: &mut [u8]) {
+    draw_line(p0, p1, col, frame);
+    draw_line(p1, p2, col, frame);
+    draw_line(p2, p0, col, frame);
+}
+
+fn clear(frame: &mut [u8]) {
+    frame.iter_mut().for_each(|m| *m = 0);
+}
+
+fn draw(frame: &mut [u8], time: Instant) {
+    //let now = Instant::now();
+    //let elapsed = now.duration_since(time).subsec_millis();
+    clear(frame);
+    draw_wire_triangle([100, 100], [200, 100], [50, 400], [0xff; 4], frame);
 }
 
 fn main() -> Result<(), Error> {
@@ -106,7 +155,7 @@ fn main() -> Result<(), Error> {
                 pixels.resize_surface(size.width, size.height);
             }
 
-            window.request_redraw();
+            //window.request_redraw();
         }
     });
 }
